73ec199cf9b3755e8ac97d83ce8d6d4c2cbed278
Amalia Bunea <50477823+amaliabunea@users.noreply.github.com>
2021-01-07 20:10:45 +0200

    change CompanyId field to Company

--- API.GraphQL/Migrations/20210107180128_Update_CompanyId_field.Designer.cs
+++ API.GraphQL/Migrations/20210107180128_Update_CompanyId_field.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using API.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace API.GraphQL.Migrations
 {
     [DbContext(typeof(APIContext))]
-    partial class APIContextModelSnapshot : ModelSnapshot
+    [Migration("20210107180128_Update_CompanyId_field")]
+    partial class Update_CompanyId_field
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
